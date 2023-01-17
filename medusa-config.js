7537fcca5fcf13d24168c65aea8b59effe96fac6
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2023-01-18 05:43:58 +0700

    fix: setup cors for admin site

--- medusa-config.js
+++ medusa-config.js
@@ -23,7 +23,7 @@ try {
 }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "*";
+const ADMIN_CORS = process.env.ADMIN_CORS || "https://localvocal-admin.onrender.com";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "*";
