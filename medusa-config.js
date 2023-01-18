4cc379d6ac8f84ea8800afc3aa9e3af2deec49f0
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2023-01-18 07:05:54 +0700

    feat: add meilisearch integration

--- medusa-config.js
+++ medusa-config.js
@@ -33,7 +33,10 @@ const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-store";
 
 // Medusa uses Redis, so this needs configuration as well
+
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const MEILISEARCH_HOST = process.env.MEILISEARCH_HOST || "http://127.0.0.1:7700"
+const MEILISEARCH_API_KEY = process.env.MEILISEARCH_API_KEY || ""
 
 // Stripe keys
 const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "";
@@ -43,6 +46,31 @@ const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
+  {
+    resolve: `medusa-plugin-meilisearch`,
+    options: {
+      // config object passed when creating an instance
+      // of the MeiliSearch client
+      config: {
+        host: MEILISEARCH_HOST,
+        apiKey: MEILISEARCH_API_KEY,
+      },
+      settings: {
+        // index name
+        products: {
+          // MeiliSearch's setting options to be set on a particular index
+          searchableAttributes: ["title", "description", "variant_sku"],
+          displayedAttributes: [
+            "title", 
+            "description", 
+            "variant_sku", 
+            "thumbnail", 
+            "handle",
+          ],
+        },
+      },
+    },
+  },
   // Uncomment to add Stripe support.
   // You can create a Stripe account via: https://stripe.com
   // {
