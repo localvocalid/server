2d00aceceab22bcf196eddf3f53818bc7edb5e36
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2023-01-20 15:06:53 +0700

    feat: integrate with minio storage and meilisearch

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+const prettierConfig = require('./.prettierrc.js')
+
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    // Best practices
+    'dot-notation': 'error',
+    'no-else-return': 'error',
+    'no-floating-decimal': 'error',
+    'no-sequences': 'error',
+
+    // Stylistic
+    'no-useless-escape': 'off',
+    'array-bracket-spacing': 'error',
+    'computed-property-spacing': ['error', 'never'],
+    curly: 'error',
+    'no-lonely-if': 'error',
+    'no-unneeded-ternary': 'error',
+    'one-var-declaration-per-line': 'error',
+    quotes: [
+      'error',
+      'single',
+      {
+        allowTemplateLiterals: false,
+        avoidEscape: true,
+      },
+    ],
+
+    // ES6
+    'array-callback-return': 'off',
+    'prefer-const': 'error',
+
+    // Typescript
+    '@typescript-eslint/no-explicit-any': ['off'],
+    'prettier/prettier': ['error', prettierConfig],
+  },
+}
--- medusa-config.js
+++ medusa-config.js
@@ -1,94 +1,65 @@
 const dotenv = require('dotenv')
 
-let ENV_FILE_NAME = '';
-switch (process.env.NODE_ENV) {
-	case 'production':
-		ENV_FILE_NAME = '.env.production';
-		break;
-	case 'staging':
-		ENV_FILE_NAME = '.env.staging';
-		break;
-	case 'test':
-		ENV_FILE_NAME = '.env.test';
-		break;
-	case 'development':
-	default:
-		ENV_FILE_NAME = '.env';
-		break;
-}
-
 try {
-	dotenv.config({ path: process.cwd() + '/' + ENV_FILE_NAME });
+  dotenv.config({ path: process.cwd() + '/.env' })
 } catch (e) {
+  console.log(e)
 }
 
-// CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "https://admin.localvocal.id";
-
-// CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "https://localvocal.id,https://www.localvocal.id,http://localhost:8000";
+// CORS
+const ADMIN_CORS = process.env.ADMIN_CORS || 'https://admin.localvocal.id'
+const STORE_CORS = process.env.STORE_CORS || 'https://localvocal.id,https://www.localvocal.id,http://localhost:8000'
 
 // Database URL (here we use a local database called medusa-development)
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost/medusa-store'
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
 
-// Medusa uses Redis, so this needs configuration as well
+// Minio
+const MINIO_ENDPOINT = process.env.MINIO_ENDPOINT || 'http://127.0.0.1:9090'
+const MINIO_BUCKET = process.env.MINIO_BUCKET || ''
+const MINIO_ACCESS_KEY = process.env.MINIO_ACCESS_KEY || ''
+const MINIO_SECRET_KEY = process.env.MINIO_SECRET_KEY || ''
 
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
-const MEILISEARCH_HOST = process.env.MEILISEARCH_HOST || "http://127.0.0.1:7700"
-const MEILISEARCH_API_KEY = process.env.MEILISEARCH_API_KEY || ""
+// Meilisearch
+const MEILISEARCH_HOST = process.env.MEILISEARCH_HOST || 'http://127.0.0.1:7700'
+const MEILISEARCH_API_KEY = process.env.MEILISEARCH_API_KEY || ''
 
-// Stripe keys
-const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "";
-const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
-
-// This is the place to include plugins. See API documentation for a thorough guide on plugins.
 const plugins = [
-  `medusa-fulfillment-manual`,
-  `medusa-payment-manual`,
-  // {
-  //   resolve: `medusa-plugin-meilisearch`,
-  //   options: {
-  //     // config object passed when creating an instance
-  //     // of the MeiliSearch client
-  //     config: {
-  //       host: MEILISEARCH_HOST,
-  //       apiKey: MEILISEARCH_API_KEY,
-  //     },
-  //     settings: {
-  //       // index name
-  //       products: {
-  //         // MeiliSearch's setting options to be set on a particular index
-  //         searchableAttributes: ["title", "description", "variant_sku"],
-  //         displayedAttributes: [
-  //           "title", 
-  //           "description", 
-  //           "variant_sku", 
-  //           "thumbnail", 
-  //           "handle",
-  //         ],
-  //       },
-  //     },
-  //   },
-  // },
-  // Uncomment to add Stripe support.
-  // You can create a Stripe account via: https://stripe.com
-  // {
-  //   resolve: `medusa-payment-stripe`,
-  //   options: {
-  //     api_key: STRIPE_API_KEY,
-  //     webhook_secret: STRIPE_WEBHOOK_SECRET,
-  //   },
-  // },
-];
+  'medusa-fulfillment-manual',
+  'medusa-payment-manual',
+  {
+    resolve: 'medusa-file-minio',
+    options: {
+      endpoint: MINIO_ENDPOINT,
+      bucket: MINIO_BUCKET,
+      access_key_id: MINIO_ACCESS_KEY,
+      secret_access_key: MINIO_SECRET_KEY,
+    },
+  },
+  {
+    resolve: 'medusa-plugin-meilisearch',
+    options: {
+      config: {
+        host: MEILISEARCH_HOST,
+        apiKey: MEILISEARCH_API_KEY,
+      },
+      settings: {
+        products: {
+          searchableAttributes: ['title', 'description', 'variant_sku'],
+          displayedAttributes: ['title', 'description', 'variant_sku', 'thumbnail', 'handle'],
+        },
+      },
+    },
+  },
+]
 
 module.exports = {
   projectConfig: {
     redis_url: REDIS_URL,
     database_url: DATABASE_URL,
-    database_type: "postgres",
+    database_type: 'postgres',
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
   },
   plugins,
-};
+}
